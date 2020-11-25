e432593c8ed317e704d31b76019e8f0663239b5d
Pablo4U <parvaka2@gmail.com>
2020-11-25 17:28:47 +0300

    Первый commit готового проекта

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class Ads : MonoBehaviour {
+  
+    private Coroutine showAd;
+
+    private string gameId = "3704357";
+    private string type = "video";
+    private bool testMode = true, needToStop;
+    private static int countLoses;
+     
+    private void Start() {
+        Advertisement.Initialize(gameId, testMode);
+        countLoses++;
+        if (countLoses % 3 == 0 ) {
+            showAd = StartCoroutine(ShowAd());
+        }    
+    }
+
+    private void Update() {
+        if (needToStop) {
+            needToStop = false;
+            StopCoroutine(showAd);
+        }
+    }
+
+    IEnumerator ShowAd() {
+        while(true) {
+            if(Advertisement.IsReady(type)) {
+                Advertisement.Show(type);
+                StopCoroutine(showAd);
+            } 
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Assets/Scripts/IsEnabled.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsEnabled : MonoBehaviour
+{
+    
+    public int  needToUnlock;
+    public Material blackMaterial;
+
+    private void Start () {
+        if (PlayerPrefs.GetInt("score") < needToUnlock ) {
+            GetComponent<MeshRenderer>().material = blackMaterial;
+        } 
+    }
+
+}
